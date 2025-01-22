5b6bf912c3930eafe9ae721cf9e5dd9166f57a27
chanu <cw8881@naver.com>
2025-01-22 14:29:51 +0900

    #1 [add] : add main navigator

---
+++ app/src/main/java/com/chanu/photocache/feature/main/MainNavigator.kt
@@ -0,0 +1,32 @@
+package com.chanu.photocache.feature.main
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.navigation.NavDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.chanu.photocache.core.navigation.Route
+
+class MainNavigator(
+    val navController: NavHostController,
+) {
+    private val currentDestination: NavDestination?
+        @Composable get() = navController
+            .currentBackStackEntryAsState().value?.destination
+
+    val startDestination = Route.Home
+
+    fun isBackStackNotEmpty(): Boolean = navController.previousBackStackEntry != null
+
+    fun navigateUp() {
+        navController.navigateUp()
+    }
+}
+
+@Composable
+internal fun rememberMainNavigator(
+    navController: NavHostController = rememberNavController(),
+): MainNavigator = remember(navController) {
+    MainNavigator(navController)
+}
