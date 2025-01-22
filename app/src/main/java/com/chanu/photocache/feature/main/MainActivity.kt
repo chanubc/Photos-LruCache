669e32f8c7e42c1250f37422aa74d5dc53e26dcf
chanu <cw8881@naver.com>
2025-01-22 11:16:52 +0900

    #1 [move] : move directory

--- app/src/main/java/com/chanu/photocache/feature/main/MainActivity.kt
+++ app/src/main/java/com/chanu/photocache/feature/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.chanu.photocache
+package com.chanu.photocache.feature.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.chanu.photocache.ui.theme.PhotoCacheTheme
+import com.chanu.photocache.core.designsystem.theme.PhotoCacheTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
