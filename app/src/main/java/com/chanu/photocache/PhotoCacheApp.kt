dc8e3ab8745de901a600a6a0c60e951cba4a0961
chanu <cw8881@naver.com>
2025-01-22 14:42:06 +0900

    #1 [add] : set timber

--- app/src/main/java/com/chanu/photocache/PhotoCacheApp.kt
+++ app/src/main/java/com/chanu/photocache/PhotoCacheApp.kt
@@ -1,9 +1,16 @@
 package com.chanu.photocache
 
 import android.app.Application
+import timber.log.Timber
 
 class PhotoCacheApp : Application() {
     override fun onCreate() {
         super.onCreate()
+        setTimber()
+    }
+
+    private fun setTimber() {
+        if (BuildConfig.DEBUG)
+            Timber.plant(Timber.DebugTree())
     }
 }
