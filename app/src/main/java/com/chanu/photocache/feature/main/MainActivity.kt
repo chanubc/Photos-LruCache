e6d3fe6b7199aa73d61734cc0522638ab2b5c41e
chanu <cw8881@naver.com>
2025-01-23 00:06:40 +0900

    #1 [feat] : set navigator on mani activity

--- app/src/main/java/com/chanu/photocache/feature/main/MainActivity.kt
+++ app/src/main/java/com/chanu/photocache/feature/main/MainActivity.kt
@@ -3,45 +3,16 @@ package com.chanu.photocache.feature.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.chanu.photocache.core.designsystem.theme.PhotoCacheTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             PhotoCacheTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val navigator: MainNavigator = rememberMainNavigator()
+                MainScreen(navigator = navigator)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PhotoCacheTheme {
-        Greeting("Android")
     }
 }
