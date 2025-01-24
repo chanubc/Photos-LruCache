020e6867bff31b20c54e8f981670c15794376522
chanu <cw8881@naver.com>
2025-01-24 20:50:31 +0900

    #3 [add] : add android entry point

--- app/src/main/java/com/chanu/photocache/feature/main/MainActivity.kt
+++ app/src/main/java/com/chanu/photocache/feature/main/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.chanu.photocache.core.designsystem.theme.PhotoCacheTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
