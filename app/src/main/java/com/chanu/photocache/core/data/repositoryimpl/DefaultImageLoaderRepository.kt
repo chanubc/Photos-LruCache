d831aab12c7f88ab116e4047ad84715bc070e1b9
chanu <cw8881@naver.com>
2025-01-24 22:10:27 +0900

    #5 [feat] : image loader 로직 구현 메모리, 디스크, 네트워크 요청 순으로 구성

---
+++ app/src/main/java/com/chanu/photocache/core/data/repositoryimpl/DefaultImageLoaderRepository.kt
@@ -0,0 +1,56 @@
+package com.chanu.photocache.core.data.repositoryimpl
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import com.chanu.photocache.cache.datasource.DiskCache
+import com.chanu.photocache.cache.datasource.MemoryCache
+import com.chanu.photocache.core.data.repository.ImageLoaderRepository
+import java.net.HttpURLConnection
+import java.net.URL
+import javax.inject.Inject
+
+class DefaultImageLoaderRepository @Inject constructor(
+    private val memoryCache: MemoryCache,
+    private val diskCache: DiskCache,
+) : ImageLoaderRepository {
+    override suspend fun loadImage(url: String): Result<Bitmap?> = runCatching {
+        // 1. 메모리 캐시에서 검색
+        val memoryCachedBitmap = memoryCache.get(url)
+        if (memoryCachedBitmap != null) {
+            return@runCatching memoryCachedBitmap
+        }
+
+        // 2. 디스크 캐시에서 검색
+        val diskCachedBitmap = diskCache.get(url)
+        if (diskCachedBitmap != null) {
+            memoryCache.put(url, diskCachedBitmap) // 메모리 캐시에 저장
+            return@runCatching diskCachedBitmap
+        }
+
+        // 3. 네트워크 요청으로 다운로드
+        val bitmap = fetchBitmapFromUrl(url)
+        if (bitmap != null) {
+            memoryCache.put(url, bitmap) // 메모리 캐시에 저장
+            diskCache.put(url, bitmap) // 디스크 캐시에 저장
+        }
+
+        return@runCatching bitmap // 결과 반환
+    }
+
+    private fun fetchBitmapFromUrl(urlString: String): Bitmap? {
+        return try {
+            // URL 객체 생성
+            val url = URL(urlString)
+            val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+            connection.doInput = true
+            connection.connect()
+
+            // InputStream을 use로 처리
+            connection.inputStream.use { inputStream ->
+                BitmapFactory.decodeStream(inputStream)
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+}
