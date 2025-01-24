063965d750a20ba167c678ce304bbd246d228de0
chanu <cw8881@naver.com>
2025-01-24 20:44:34 +0900

    #3 [feat] : make home repository with pager

---
+++ app/src/main/java/com/chanu/photocache/core/data/repository/HomeRepository.kt
@@ -0,0 +1,9 @@
+package com.chanu.photocache.core.data.repository
+
+import androidx.paging.PagingData
+import com.chanu.photocache.core.model.PhotoModel
+import kotlinx.coroutines.flow.Flow
+
+interface HomeRepository {
+    fun getPhotos(): Flow<PagingData<PhotoModel>>
+}
