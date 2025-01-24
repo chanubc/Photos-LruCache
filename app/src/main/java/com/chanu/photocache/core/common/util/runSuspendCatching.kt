199b4147a28baedd68bd83995bc990617a553998
chanu <cw8881@naver.com>
2025-01-24 23:39:41 +0900

    #5 [feat] : add run suspend catching for CancellationException

---
+++ app/src/main/java/com/chanu/photocache/core/common/util/runSuspendCatching.kt
@@ -0,0 +1,13 @@
+package com.chanu.photocache.core.common.util
+
+import kotlin.coroutines.cancellation.CancellationException
+
+suspend inline fun <R> runSuspendCatching(block: () -> R): Result<R> {
+    return try {
+        Result.success(block())
+    } catch (c: CancellationException) {
+        throw c
+    } catch (e: Throwable) {
+        Result.failure(e)
+    }
+}
