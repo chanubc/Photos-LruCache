de32d4fd93e3e6f5c58aecaf86a91f600395d634
chanu <cw8881@naver.com>
2025-01-26 20:01:46 +0900

    #9 [mod] : 전체 스낵바 메세지 에러 핸들링 로직 수정, 사용자에게 불필요한 메세지 감춤

--- app/src/main/java/com/chanu/photocache/core/common/util/GetThrowableMessage.kt
+++ app/src/main/java/com/chanu/photocache/core/common/util/GetThrowableMessage.kt
@@ -1,11 +1,12 @@
 package com.chanu.photocache.core.common.util
 
+import android.content.res.Resources
+import com.chanu.photocache.R
 import com.chanu.photocache.core.model.CustomError
 
-fun getThrowableMessage(throwable: Throwable?): String = when (throwable) {
-    is CustomError.UnknownError -> throwable.message.toString()
-    is CustomError.NetWorkConnectError -> throwable.message.toString()
-    is CustomError.ApiError -> throwable.message.toString()
-    is CustomError.TimeOutError -> throwable.message.toString()
+fun getThrowableMessage(
+    throwable: Throwable?, localContextResource: Resources,
+): String = when (throwable) {
+    is CustomError.UnknownError -> localContextResource.getString(R.string.error_message_unknown)
     else -> throwable?.message.toString()
 }
