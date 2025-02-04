faffca326fd3509b9a086dce73a88fcc4a37ed68
chanu <cw8881@naver.com>
2025-02-04 18:12:13 +0900

    #13 [hotfix] : 사용하는 클래스로 수정

--- app/src/main/java/com/chanu/photocache/core/data/repositoryimpl/DefaultImageLoaderRepository.kt
+++ app/src/main/java/com/chanu/photocache/core/data/repositoryimpl/DefaultImageLoaderRepository.kt
@@ -1,10 +1,10 @@
 package com.chanu.photocache.core.data.repositoryimpl
 
 import android.graphics.Bitmap
-import com.chanu.photocache.cache.datasource.CoroutineLRUDiskCache2
 import com.chanu.photocache.core.common.util.runSuspendCatching
 import com.chanu.photocache.core.common.util.toGenerateKey
 import com.chanu.photocache.core.data.cache.datasource.BitmapFetcher
+import com.chanu.photocache.core.data.cache.datasource.DiskCache
 import com.chanu.photocache.core.data.cache.datasource.MemoryCache
 import com.chanu.photocache.core.data.repository.ImageLoaderRepository
 import com.chanu.photocache.core.data.util.handleThrowable
@@ -12,7 +12,7 @@ import javax.inject.Inject
 
 class DefaultImageLoaderRepository @Inject constructor(
     private val memoryCache: MemoryCache,
-    private val diskCache: CoroutineLRUDiskCache2,
+    private val diskCache: DiskCache,
     private val bitmapFetcher: BitmapFetcher,
 ) : ImageLoaderRepository {
     // 메모리 캐시, 디스크 캐시, 네트워크 순서로 이미지 조회
